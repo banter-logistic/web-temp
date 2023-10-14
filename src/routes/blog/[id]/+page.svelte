473b26754c5ad5d4ad3f604727c97bdf69fb6768
RyoDian <111202113852@mhs.dinus.ac.id>
2023-10-14 16:13:01 +0700

    penambhan logo

--- src/routes/blog/[id]/+page.svelte
+++ src/routes/blog/[id]/+page.svelte
@@ -5,28 +5,33 @@ export let data
 
 </script>
 
-  <div class="w-[85%] mx-auto pt-16">
+  <div class="w-[85%] mx-auto pt-12">
  <div class="container">
-  <div class="lg:float-right float-none md:pl-8 pl-0 pb-8 ">
+  <div class="lg:float-right float-none md:pl-8 pl-0 ">
           <img
             src={data.res.img}
             alt=""
-            class="object-cover w-full h-[20rem]  rounded-lg"
+            class="object-cover w-[40rem] h-[20rem]  rounded-lg"
           />
         </div>
       <div class="">
         <div class="flex flex-col gap-16">
-          <h1 class="font-bold md:text-2xl sm:text-xl text-xl">{data.res.title}</h1>
+          <h1 class="font-bold md:text-2xl sm:text-xl text-xl pt-8">{data.res.title}</h1>
            
 
         </div>
        
       </div>
-      <div class="pt-8 ">
-        <p class="text-justify leading-6 indent-12 ">
+      <div class="pt-4 ">
+        <!-- <p class="text-justify leading-6 indent-12 ">
           {data.res.content}
+        </p> -->
+        {#each data.res.content.split('\n') as d}
+        <p class="text-justify leading-6 indent-12 ">
+          {d}
         </p>
-           
+        {/each}
+ 
       </div>
     </div>
 
